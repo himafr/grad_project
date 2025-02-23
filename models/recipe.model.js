f935d528d4b44d36813a20ee1ef47ec6e0be77f5
hima <hemawith4@gmail.com>
2025-02-23 23:43:22 +0200

    id with recipes relation

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -32,7 +32,7 @@ class RecipesModel {
   }
 
   static async getRecipeById(id) {
-    const sql = "SELECT * FROM FROM recipes INNER JOIN categories ON recipes.category_id = categories.category_id WHERE recipe_id = ?";
+    const sql = "SELECT * FROM recipes INNER JOIN categories ON recipes.category_id = categories.category_id WHERE recipe_id = ?";
     const reviewsSql = "SELECT * FROM recipe_review INNER JOIN users ON users.user_id=recipe_review.user_id WHERE recipe_id = ?";
     const commentsSql = "SELECT * FROM recipe_comments INNER JOIN users ON users.user_id=recipe_comments.user_id WHERE recipe_id = ?";
     const params = [id];
