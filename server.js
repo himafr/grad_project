92b36f46a533f3f0a25cd24cf97c9253845ea5c5
hima <hemawith4@gmail.com>
2025-02-27 04:35:23 +0200

    access origin headers

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 require('dotenv').config();
-const http = require('http');  // Importing http module
 const { Database } = require('./config/db.config');
+const app = require('./app');
+const http = require('http').createServer(app);  
+const socketIo=require('socket.io')(http,{
+    cors:{
+        origin:"*"
+    }
+});
+// Importing http module
 
 // Uncomment for error handling
 // process.on('uncaughtException', err => {
-//     console.log("UNCAUGHT EXCEPTION!!!", err.message);
-//     process.exit(1);
-// });
+    //     console.log("UNCAUGHT EXCEPTION!!!", err.message);
+    //     process.exit(1);
+    // });
+    
+    
 
-
-const {Server} = require('socket.io');
-const app = require('./app');
-
-const server = http.createServer(app)
- const io = new Server(server, {
-    cors: {
-      origin: '*',
-      methods: ["GET", "POST"],
-      // allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-  })
+ 
 // const serve = http.createServer(app);
 // const io = socketIo(serve, {
 //     cors: {
@@ -43,24 +40,27 @@ function formatDate(date) {
 }
 const activeUsers=[2,2];
 
-io.on('connect', (socket) => {
-    
-    console.log('a user connected');
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    socket.on('user active', async (user) => {
-        console.log(user)
-        const userActivity = await Database.executeQuery(`UPDATE users SET last_active = NOW() WHERE user_id = ?`,[user.userId])
-    
-    });
-});
 
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`Server running on ${port}....`));
+http.listen(port, () => {console.log(`Server running on ${port}....`)
+    socketIo.on('connect', (socket) => {
+    
+        console.log('a user connected');
+    
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    
+        socket.on('user active', async (user) => {
+            console.log(user)
+            const userActivity = await Database.executeQuery(`UPDATE users SET last_active = NOW() WHERE user_id = ?`,[user.userId])
+        
+        });
+    });
+
+});
 
 // Uncomment for error handling
 // process.on('unhandledRejection', err => {
